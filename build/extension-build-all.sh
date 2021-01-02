ad3f3da59e8260b992f38928fdd0e32d4a9458b2
Yogi1967 <sbeesley@gmail.com>
2021-01-02 18:40:11 +0000

    Restructuring project folders

--- build/extension-build-all.sh
+++ build/extension-build-all.sh
@@ -2,56 +2,43 @@
 
 clear
 
-if [ "$1" = "" ] ; then
-  echo "@@@ NO PARAMETERS SUPPLIED."
-  echo "Run from project root. Specify test or final"
-  exit 1
-fi
-
 if ! test -f "./build/extension-build.sh"; then
     echo "@@ PLEASE RUN FROM THE PROJECT's ROOT directory! @@"
     exit 1
 fi
 
-if  [ "$1" != "test" ] && [ "$1" != "final" ] ; then
-  echo
-  echo "@@ Incorrect build type @@"
-  echo "must be: test or final"
-  exit 1
-fi
-
-./build/extension-build.sh toolbox $1
+./build/extension-build.sh toolbox
 if [ $? -ne 0 ]; then
     echo *** BUILD Failed??
     read -p "Press any key to resume next build..."
 fi
 
-./build/extension-build.sh stockglance2020 $1
+./build/extension-build.sh stockglance2020
 if [ $? -ne 0 ]; then
     echo *** BUILD Failed??
     read -p "Press any key to resume next build..."
 fi
 
-./build/extension-build.sh extract_reminders_csv $1
+./build/extension-build.sh extract_reminders_csv
 if [ $? -ne 0 ]; then
     echo *** BUILD Failed??
     read -p "Press any key to resume next build..."
 fi
 
-./build/extension-build.sh extract_investment_transactions_csv $1
+./build/extension-build.sh extract_investment_transactions_csv
 if [ $? -ne 0 ]; then
     echo *** BUILD Failed??
     read -p "Press any key to resume next build..."
 fi
 
-./build/extension-build.sh extract_currency_history_csv $1
+./build/extension-build.sh extract_currency_history_csv
 if [ $? -ne 0 ]; then
     echo *** BUILD Failed??
     read -p "Press any key to resume next build..."
 fi
 
 
-./build/extension-build.sh extract_account_registers_csv $1
+./build/extension-build.sh extract_account_registers_csv
 if [ $? -ne 0 ]; then
     echo *** BUILD Failed??
     read -p "Press any key to resume next build..."
--- build/extension-build.sh
+++ build/extension-build.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-clear
+echo
+echo
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo @@@@ BUILD of $1 RUNNING
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo
+echo
 
 if [ "$1" = "" ] ; then
   echo "@@@ NO PARAMETERS SUPPLIED."
   echo "Run from project root"
-  echo "Usage ./build/extension-build.sh module_name build_type"
+  echo "Usage ./build/extension-build.sh module_name"
   echo "Module name must be one of: toolbox, stockglance2020, extract_reminders_csv, extract_investment_transactions_csv, extract_currency_history_csv, extract_account_registers_csv"
-  echo "Build type must be: test or final"
   exit 1
 fi
 
@@ -25,27 +30,20 @@ else
     FILE=$1
 fi
 
-if  [ "$2" != "test" ] && [ "$2" != "final" ] ; then
-  echo
-  echo "@@ Incorrect build type @@"
-  echo "must be: test or final"
-  exit 1
-fi
-
 # Relies on "ant genkeys" and various jar files from within the Moneydance devkit
 
-if ! test -f "./source/$FILE.py"; then
-    echo "ERROR - $FILE.py does not exist!"
+if ! test -f "./source/$FILE/$FILE.py"; then
+    echo "ERROR - $FILE/$FILE.py does not exist!"
     exit 1
 fi
 
-if ! test -f "./source/$FILE-script_info.dict"; then
-    echo "ERROR - $FILE-script_info.dict does not exist!"
+if ! test -f "./source/$FILE/script_info.dict"; then
+    echo "ERROR - $FILE/script_info.dict does not exist!"
     exit 1
 fi
 
-if ! test -f "./source/$FILE-meta_info.dict"; then
-    echo "ERROR - $FILE-meta_info.dict does not exist!"
+if ! test -f "./source/$FILE/meta_info.dict"; then
+    echo "ERROR - $FILE/meta_info.dict does not exist!"
     exit 1
 fi
 
@@ -75,16 +73,8 @@ if ! test -f "./source/install-readme.txt"; then
 fi
 
 
-if test -f "./source/$FILE.mxt"; then
-    rm "./source/$FILE.mxt"
-fi
-
-if test -f "./source/script_info.dict"; then
-    rm ./source/script_info.dict
-fi
-
-if test -f "./source/meta_info.dict"; then
-    rm ./source/meta_info.dict
+if test -f "./source/$FILE/$FILE.mxt"; then
+    rm "./source/$FILE/$FILE.mxt"
 fi
 
 if ! test -d "com"; then
@@ -107,21 +97,20 @@ if ! test -d "com/moneydance/modules/features/$FILE"; then
     mkdir com/moneydance/modules/features/"$FILE"
 fi
 
-cp "./source/$FILE-script_info.dict" ./source/script_info.dict
-cp "./source/$FILE-meta_info.dict" com/moneydance/modules/features/"$FILE"/meta_info.dict
+cp "./source/$FILE/meta_info.dict" "com/moneydance/modules/features/$FILE/meta_info.dict"
 
-zip -j -z ./source/"$FILE".mxt "./source/$FILE".py <<< "StuWareSoftSystems: $FILE Python Extension for Moneydance (by Stuart Beesley). Please see install-readme.txt"
+zip -j -z "./source/$FILE/$FILE.mxt" "./source/$FILE/$FILE.py" <<< "StuWareSoftSystems: $FILE Python Extension for Moneydance (by Stuart Beesley). Please see install-readme.txt"
 
-if test -f "./source/$FILE"-readme.txt; then
-  zip -j ./source/"$FILE".mxt ./source/"$FILE"-readme.txt
+if test -f "./source/$FILE/readme.txt"; then
+  zip -j "./source/$FILE/$FILE.mxt" "./source/$FILE/readme.txt"
 else
-  echo "No help file to ZIP - skipping....."
+  echo "No readme.txt file to ZIP - skipping....."
 fi
 
-zip -j ./source/"$FILE.mxt" "./source/install-readme.txt"
+zip -j "./source/$FILE/$FILE.mxt" "./source/install-readme.txt"
 
-zip -m -j ./source/"$FILE.mxt" ./source/script_info.dict
-zip -m ./source/"$FILE.mxt" "com/moneydance/modules/features/$FILE/meta_info.dict"
+zip -j "./source/$FILE/$FILE.mxt" "./source/$FILE/script_info.dict"
+zip -m "./source/$FILE/$FILE.mxt" "com/moneydance/modules/features/$FILE/meta_info.dict"
 
 cp "./moneydance-devkit-5.1 2/lib/extadmin.jar" .
 cp "./moneydance-devkit-5.1 2/lib/moneydance-dev.jar" .
@@ -129,17 +118,17 @@ cp "./moneydance-devkit-5.1 2/lib/moneydance-dev.jar" .
 cp "./moneydance-devkit-5.1 2/src/priv_key" .
 cp "./moneydance-devkit-5.1 2/src/pub_key" .
 
-if test -f ./source/"s-$FILE.mxt"; then
-    rm ./source/"s-$FILE.mxt"
+if test -f "./source/$FILE/s-$FILE.mxt"; then
+    rm "./source/$FILE/s-$FILE.mxt"
 fi
 
-if test -f ./"s-$FILE.mxt"; then
-    rm ./"s-$FILE.mxt"
+if test -f "./s-$FILE.mxt"; then
+    rm "./s-$FILE.mxt"
 fi
 
 #read -p "Press any key to resume ..."
 
-java -cp extadmin.jar:moneydance-dev.jar com.moneydance.admin.KeyAdmin signextjar priv_key private_key_id "$FILE" ./source/"$FILE.mxt" < "./build/extension_keyfile."
+java -cp extadmin.jar:moneydance-dev.jar com.moneydance.admin.KeyAdmin signextjar priv_key private_key_id "$FILE" "./source/$FILE/$FILE.mxt" < "./build/extension_keyfile."
 if ! test -f "s-$FILE.mxt"; then
     echo "ERROR - Signed MXT does not exist?"
     exit 3
@@ -154,60 +143,55 @@ rm pub_key
 rm extadmin.jar
 rm moneydance-dev.jar
 
-ls -l ./source/"$FILE".mxt
+ls -l "./source/$FILE/$FILE.mxt"
 ls -l "s-$FILE.mxt"
 
-rm ./source/"$FILE".mxt
-mv "s-$FILE.mxt" ./source/"$FILE.mxt"
+rm "./source/$FILE/$FILE.mxt"
+mv "s-$FILE.mxt" "./source/$FILE/$FILE.mxt"
 
 
-if test -f ./source/"$FILE.mxt"; then
-    ls -l ./source/"$FILE".mxt
-    unzip -l ./source/"$FILE".mxt
+if test -f "./source/$FILE/$FILE.mxt"; then
+    ls -l "./source/$FILE/$FILE.mxt"
+    unzip -l "./source/$FILE/$FILE.mxt"
     echo ===================
-    echo FILE ./source/"$FILE".mxt has been built using "$2" mode...
+    echo FILE "./source/$FILE/$FILE.mxt" has been built...
 else
     echo @@@@@@@@@@@@@@@@@
-    echo PROBLEM CREATING ./source/"$FILE".mxt - "$2" mode...
+    echo PROBLEM CREATING "./source/$FILE/$FILE.mxt" ...
     exit 4
 fi
 
-if  [ "$2" != "final" ] ; then
-  echo TESTING - your built "$FILE.mxt" will be in the ./source directory
-  exit 0
-fi
-
 echo @ Building final ZIP file for publication...
-if test -f ./"$FILE.zip"; then
+if test -f "./$FILE.zip"; then
     echo "Removing old ./$FILE.zip"
-    rm ./"$FILE.zip"
+    rm "./$FILE.zip"
 fi
 
-zip -j -z ./"$FILE.zip" "./source/install-readme.txt" <<< "StuWareSoftSystems: $FILE for Moneydance (by Stuart Beesley). Please see install-readme.txt"
-zip -j -c ./"$FILE.zip" "./source/$FILE.mxt" <<< "StuWareSoftSystems: $FILE Extension for Moneydance."
+zip -j -z "./$FILE.zip" "./source/install-readme.txt" <<< "StuWareSoftSystems: $FILE for Moneydance (by Stuart Beesley). Please see install-readme.txt"
+zip -j -c "./$FILE.zip" "./source/$FILE/$FILE.mxt" <<< "StuWareSoftSystems: $FILE Extension for Moneydance."
 
 
 if  [ "$1" != "toolbox" ] ; then
-  zip -j -c ./"$FILE.zip" "./source/$FILE.py" <<< "StuWareSoftSystems: $FILE Script for Moneydance."
+  zip -j -c "./$FILE.zip" "./source/$FILE/$FILE.py" <<< "StuWareSoftSystems: $FILE Script for Moneydance."
 else
   echo "Not including $FILE.py for toolbox package...."
 fi
 
-if test -f "./source/$FILE"-readme.txt; then
-  zip -j ./"$FILE".zip ./source/"$FILE"-readme.txt
+if test -f "./source/$FILE/readme.txt"; then
+  zip -j "./$FILE.zip" "./source/$FILE/readme.txt"
 else
   echo "No help file to ZIP - skipping....."
 fi
 
 
-if test -f ./"$FILE.zip"; then
-    ls -l ./"$FILE".zip
-    unzip -l ./"$FILE".zip
+if test -f "./$FILE.zip"; then
+    ls -l "./$FILE.zip"
+    unzip -l "./$FILE.zip"
     echo ===================
-    echo DISTRIBUTION FILE ./"$FILE".zip has been built...
+    echo DISTRIBUTION FILE "./$FILE.zip" has been built...
 else
     echo @@@@@@@@@@@@@@@@@
-    echo PROBLEM CREATING FINAL DISTRIBUTION ./"$FILE".zip !
+    echo PROBLEM CREATING FINAL DISTRIBUTION "./$FILE.zip" !
     exit 5
 fi
 
