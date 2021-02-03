2b18752d404d8f39b99b304e142a2b84e2c138f1
Yogi1967 <sbeesley@gmail.com>
2021-02-03 11:01:41 +0000

    Tweaks / updates.

--- build/extension-build-all.sh
+++ build/extension-build-all.sh
@@ -9,42 +9,42 @@ fi
 
 ./build/extension-build.sh toolbox
 if [ $? -ne 0 ]; then
-    echo *** BUILD Failed??
+    echo "*** BUILD Failed??"
     read -p "Press any key to resume next build..."
 fi
 
 ./build/extension-build.sh stockglance2020
 if [ $? -ne 0 ]; then
-    echo *** BUILD Failed??
+    echo "*** BUILD Failed??"
     read -p "Press any key to resume next build..."
 fi
 
 ./build/extension-build.sh extract_reminders_csv
 if [ $? -ne 0 ]; then
-    echo *** BUILD Failed??
+    echo "*** BUILD Failed??"
     read -p "Press any key to resume next build..."
 fi
 
 ./build/extension-build.sh extract_investment_transactions_csv
 if [ $? -ne 0 ]; then
-    echo *** BUILD Failed??
+    echo "*** BUILD Failed??"
     read -p "Press any key to resume next build..."
 fi
 
 ./build/extension-build.sh extract_currency_history_csv
 if [ $? -ne 0 ]; then
-    echo *** BUILD Failed??
+    echo "*** BUILD Failed??"
     read -p "Press any key to resume next build..."
 fi
 
 ./build/extension-build.sh extract_account_registers_csv
 if [ $? -ne 0 ]; then
-    echo *** BUILD Failed??
+    echo "*** BUILD Failed??"
     read -p "Press any key to resume next build..."
 fi
 
 ./build/extension-build.sh useful_scripts
 if [ $? -ne 0 ]; then
-    echo *** BUILD Failed??
+    echo "*** BUILD Failed??"
     read -p "Press any key to resume next build..."
 fi
