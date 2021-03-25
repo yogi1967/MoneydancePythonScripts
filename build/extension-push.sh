86c3754c265d7d2dd8a22e13896943d0d1e94ddb
Yogi1967 <sbeesley@gmail.com>
2021-03-25 20:54:00 +0000

    Updated usaa fix pdf...

--- build/extension-push.sh
+++ build/extension-push.sh
@@ -5,7 +5,7 @@ PUSHDIR="../MoneydanceOpen/python_scripts"
 echo
 echo
 echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-echo @@@@ PUSH - RUNNING to $PUSHDIR - module: "$1"
+echo @@@@ PUSH - RUNNING to ${PUSHDIR} - module: "$1"
 echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 echo
 echo
@@ -32,18 +32,18 @@ fi
 
 MODULE=$1
 
-if ! test -d "{$PUSHDIR}"; then
-  echo "@@ {$PUSHDIR} directory missing? @@"
+if ! test -d "${PUSHDIR}"; then
+  echo "@@ ${PUSHDIR} directory missing? @@"
   exit 1
 fi
 
-if ! test -d "{$PUSHDIR}/{$MODULE}"; then
-  echo "@@ {$PUSHDIR}/{$MODULE} directory missing? @@"
+if ! test -d "${PUSHDIR}/${MODULE}"; then
+  echo "@@ ${PUSHDIR}/${MODULE} directory missing? @@"
   exit 1
 fi
 
-if ! test -f "./{$MODULE}.zip"; then
-  echo "@@ ./{$MODULE}.zip missing? @@"
+if ! test -f "./${MODULE}.zip"; then
+  echo "@@ ./${MODULE}.zip missing? @@"
   exit 2
 fi
 
@@ -52,43 +52,43 @@ if [ "${MODULE}" = "useful_scripts" ]; then
   echo "Skipping checks for non-extension"
 
 else
-  if ! test -f "./source/{$MODULE}/{$MODULE}.py"; then
-    echo "@@ ./source/{$MODULE}/{$MODULE}.py missing? @@"
+  if ! test -f "./source/${MODULE}/${MODULE}.py"; then
+    echo "@@ ./source/${MODULE}/${MODULE}.py missing? @@"
     exit 2
   fi
 
-  if ! test -f "./source/{$MODULE}/meta_info.dict"; then
-    echo "@@ ./source/{$MODULE}/meta_info.dict missing? @@"
+  if ! test -f "./source/${MODULE}/meta_info.dict"; then
+    echo "@@ ./source/${MODULE}/meta_info.dict missing? @@"
     exit 2
   fi
-  if ! test -f "./source/{$MODULE}/script_info.dict"; then
-    echo "@@ ./source/{$MODULE}/script_info.dict missing? @@"
+  if ! test -f "./source/${MODULE}/script_info.dict"; then
+    echo "@@ ./source/${MODULE}/script_info.dict missing? @@"
     exit 2
   fi
 fi
 
-cp "./source/${MODULE}"/* "{$PUSHDIR}/{$MODULE}/."
+cp "./source/${MODULE}"/* "${PUSHDIR}/${MODULE}/."
 if [ $? -ne 0 ]; then
   echo "*** cp ./source/${MODULE}/* Failed??"
   exit 3
 fi
 
 if [ "${MODULE}" != "extension_tester" ]; then
-  cp "./source/install-readme.txt" "{$PUSHDIR}/{$MODULE}/."
+  cp "./source/install-readme.txt" "${PUSHDIR}/${MODULE}/."
   if [ $? -ne 0 ]; then
     echo "*** cp ./source/install-readme.txt Failed??"
     exit 4
   fi
 fi
 
-rm -f "{$PUSHDIR}/{$MODULE}"/*.docx
-rm -f "{$PUSHDIR}/{$MODULE}"/*.mxt
+rm -f "${PUSHDIR}/${MODULE}"/*.docx
+rm -f "${PUSHDIR}/${MODULE}"/*.mxt
 
 if [ "${MODULE}" = "toolbox" ]; then
-  rm "{$PUSHDIR}/{$MODULE}/toolbox_version_requirements.dict"
+  rm "${PUSHDIR}/${MODULE}/toolbox_version_requirements.dict"
 fi
 
-echo "Listing {$PUSHDIR}/{$MODULE}"
-ls -l "{$PUSHDIR}/{$MODULE}"
+echo "Listing ${PUSHDIR}/${MODULE}"
+ls -l "${PUSHDIR}/${MODULE}"
 
-echo "@@@ module: {$MODULE} pushed over to {$PUSHDIR}"
+echo "@@@ module: ${MODULE} pushed over to ${PUSHDIR}"
