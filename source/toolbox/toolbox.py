ddf674805cb2470b37fdc3e405ece8342f628869
Yogi1967 <sbeesley@gmail.com>
2021-11-25 17:51:00 +0000

    Updated Toolbox for MD2022.3(4062)

--- source/toolbox/toolbox.py
+++ source/toolbox/toolbox.py
@@ -617,7 +617,7 @@ else:
     MD_MDPLUS_BUILD = 4040                                                                                              # noqa
     TOOLBOX_MINIMUM_TESTED_MD_VERSION = 2020.0                                                                          # noqa
     TOOLBOX_MAXIMUM_TESTED_MD_VERSION = 2022.3                                                                          # noqa
-    TOOLBOX_MAXIMUM_TESTED_MD_BUILD =   4061                                                                            # noqa
+    TOOLBOX_MAXIMUM_TESTED_MD_BUILD =   4062                                                                            # noqa
     MD_OFX_BANK_SETTINGS_DIR = "https://infinitekind.com/app/md/fis/"                                                   # noqa
     MD_OFX_DEFAULT_SETTINGS_FILE = "https://infinitekind.com/app/md/fi2004.dict"                                        # noqa
     MD_OFX_DEBUG_SETTINGS_FILE = "https://infinitekind.com/app/md.debug/fi2004.dict"                                    # noqa
