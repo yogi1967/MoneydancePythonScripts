b1349b1cef60df4d90df3320ba66a71a901e7a46
Yogi1967 <sbeesley@gmail.com>
2024-04-21 21:47:44 +0100

    5118

--- launch-moneydance.sh
+++ launch-moneydance.sh
@@ -55,7 +55,7 @@ unset md_passphrase
 
 # set to "" for standard app install name (I add the version and build to the app name when installing)
 #md_version=""
-md_version=" 2024 (5117)"
+md_version=" 2024.1 (5118)"
 
 # Download/install OpenAdoptJDK (Hotspot) v15: https://adoptopenjdk.net/?variant=openjdk15&jvmVariant=hotspot
 # Download/install Java FX (allows Moneybot Console) to run: https://gluonhq.com/download/javafx-15-0-1-sdk-mac/
