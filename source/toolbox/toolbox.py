03f0cd3a576e0b01def18ad4dbd0fa1291395047
Yogi1967 <sbeesley@gmail.com>
2024-05-31 19:15:53 +0100

    Toolbox - MD build 5130

--- source/toolbox/toolbox.py
+++ source/toolbox/toolbox.py
@@ -605,8 +605,8 @@ else:
     GlobalVars.__TOOLBOX = None
 
     GlobalVars.TOOLBOX_MINIMUM_TESTED_MD_VERSION = 2020.0
-    GlobalVars.TOOLBOX_MAXIMUM_TESTED_MD_VERSION = 2024.0
-    GlobalVars.TOOLBOX_MAXIMUM_TESTED_MD_BUILD =   5117
+    GlobalVars.TOOLBOX_MAXIMUM_TESTED_MD_VERSION = 2024.2
+    GlobalVars.TOOLBOX_MAXIMUM_TESTED_MD_BUILD =   5130
     GlobalVars.MD_OFX_BANK_SETTINGS_DIR = "https://infinitekind.com/app/md/fis/"
     GlobalVars.MD_OFX_DEFAULT_SETTINGS_FILE = "https://infinitekind.com/app/md/fi2004.dict"
     GlobalVars.MD_OFX_DEBUG_SETTINGS_FILE = "https://infinitekind.com/app/md.debug/fi2004.dict"
