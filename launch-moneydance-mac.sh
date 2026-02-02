2c8b7486ea624fb62471221d1a81206a8aadd0ed
lionel <785q4tuv93@snkmail.com>
2026-02-02 11:17:50 -0700

    launch-moneydance-mac.sh: Substitute $HOME to avoid hardcoding

--- launch-moneydance-mac.sh
+++ launch-moneydance-mac.sh
@@ -54,8 +54,8 @@ echo "My user path: ${my_user_path}"
 "/Applications/Moneydance.app/Contents/MacOS/Moneydance" \
   -d \
   "$*" \
-  &>"/Users/stu/Library/Containers/com.infinitekind.MoneydanceOSX/Data/Library/Application Support/Moneydance/errlog.txt"
-#  "/Users/stu/Library/Containers/com.infinitekind.MoneydanceOSX/Data/Documents/Quote Loader Examples.moneydance" \
+  &>"$HOME/Library/Containers/com.infinitekind.MoneydanceOSX/Data/Library/Application Support/Moneydance/errlog.txt"
+#  "$HOME/Library/Containers/com.infinitekind.MoneydanceOSX/Data/Documents/Quote Loader Examples.moneydance" \
 # -invoke=moneydance:fmodule:securityquoteload:runstandalone:quit
 # -invoke=moneydance:fmodule:extract_data:autoextract:quit
 exit 0
